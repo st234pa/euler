e522d30e19c2159adc978ea043cd80376dc92f51
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-09-16 17:02:48 -0400

    finished problem 2

--- problem2.c
+++ problem2.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 int main() {
-  
+  int total, a, b, even;
+  total = 2;
+  even = 2;
+  a = 1;
+  b = 2;
+  while (b <  4*(int)(pow(10, 6))) {
+    total = a+b;
+    a = b;
+    b = total;
+    if (!(total%2)) even += total;
+  }
+  printf("Even valued terms sum to: %d", even);
+  return even;
+}
